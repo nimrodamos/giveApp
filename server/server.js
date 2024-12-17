64d91da89320b5b3d52f3bdb7ec84517f52029c6
ArielBitan <joekjoek33@gmail.com>
2024-12-17 14:00:39 +0200

    database connection

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { json } = require("express");
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+const app = express();
+
+mongoose
+  .connect(uri)
+  .then(async () => {
+    console.log("Connected to database. Checking documents...");
+  })
+  .catch((error) => {
+    console.error(
+      "Error connecting to the database or running operations:",
+      error
+    );
+  });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(json());
