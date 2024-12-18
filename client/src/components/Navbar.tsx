4bcf33336312f8bd2427e5e117b42e29611a5ae0
nimrod amos <nimrodamos100@gmail.com>
2024-12-18 14:06:31 +0200

    cro

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import LoginModal from "./LoginModal";
 import { useTheme } from "./Theme-provider";
+import { Link } from "react-router-dom";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,36 +18,38 @@ const Navbar = () => {
     <nav className="bg-background text-foreground border-b border-border shadow-sm">
       <div className="container mx-auto flex justify-between items-center p-4">
         {/* לוגו */}
-        <div className="text-2xl font-bold text-primary">GiveApp</div>
+        <Link to="/" className="text-2xl font-bold text-primary">
+          GiveApp
+        </Link>
 
         {/* תפריט */}
         <ul className="flex space-x-6 items-center">
           <li>
-            <a href="/" className="hover:text-primary">
-              Home
+            <a
+              href="/projects"
+              className="relative font-bold hover:text-primary m-4 px-4 py-2 after:absolute after:right-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-primary after:origin-right after:transition-all after:duration-300 hover:after:w-full"
+            >
+              פרויקטים
             </a>
           </li>
           <li>
-            <a href="/projects" className="hover:text-primary">
-              Projects
+            <a
+              href="/create"
+              className="relative font-bold hover:text-primary px-4 py-2 after:absolute after:right-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-primary after:origin-right after:transition-all after:duration-300 hover:after:w-full"
+            >
+              להתחיל פרויקט
             </a>
           </li>
-          <li>
-            <a href="/create" className="hover:text-primary">
-              Create Project
-            </a>
-          </li>
-
           {/* Toggle Dark Mode */}
           <li>
-            <div className="flex items-center space-x-2">
-              <span className="text-sm text-muted-foreground">
-                {theme === "light" ? "Light" : "Dark"}
-              </span>
+            <div className="flex items-center space-x-2 gap-2">
               <Switch
                 checked={theme === "dark"}
                 onCheckedChange={toggleTheme}
               />
+              <span className="text-sm text-muted-foreground ">
+                {theme === "light" ? <FiSun size={20} /> : <FiMoon size={20} />}
+              </span>
             </div>
           </li>
 
