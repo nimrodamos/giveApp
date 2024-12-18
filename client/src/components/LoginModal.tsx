4df036ef418a2afe9bbd681855ef38f8f130f049
ArielBitan <joekjoek33@gmail.com>
2024-12-18 15:50:44 +0200

    login success

--- client/src/components/LoginModal.tsx
+++ client/src/components/LoginModal.tsx
@@ -1,46 +1,85 @@
 import Modal from "react-modal";
 import { LoginModalProps } from "@/types/loginTypes";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { api } from "@/api";
+import { useNavigate } from "react-router-dom";
+
 const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("users/login", formData, {
+        withCredentials: true,
+      });
+
+      if (!response.data) {
+        console.log("error logging in");
+      }
+
+      console.log("Login successful:", response.data);
+
+      navigate("/");
+      setFormData({ email: "", password: "" });
+      onClose();
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Login Modal"
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 dark:bg-dark-overlay"
-      overlayClassName="fixed inset-0"
+      className="bg-card p-6 rounded-lg shadow-lg w-96 text-card-foreground text-center"
+      overlayClassName="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
       ariaHideApp={false}
     >
-      <div className="bg-card p-6 rounded-lg shadow-lg w-96 text-card-foreground dark:bg-dark-card dark:text-dark-card-foreground text-center">
-        <h2 className="text-2xl font-bold mb-4 text-foreground dark:text-dark-foreground">
-          Login
-        </h2>
-        <form className="space-y-4">
+      <div>
+        <h2 className="text-2xl font-bold mb-4 text-foreground">Login</h2>
+        <form onSubmit={handleOnSubmit} className="space-y-4">
           <input
-            type="email"
+            name="email"
             placeholder="Email"
-            className="w-full p-2 border rounded bg-input text-foreground placeholder-muted-foreground dark:bg-dark-input dark:text-dark-foreground dark:placeholder-dark-muted-foreground"
+            value={formData.email}
+            onChange={handleOnChange}
+            className="w-full p-2 border rounded bg-input text-foreground placeholder-muted-foreground"
+            required
           />
           <input
             type="password"
+            name="password"
             placeholder="Password"
-            className="w-full p-2 border rounded bg-input text-foreground placeholder-muted-foreground dark:bg-dark-input dark:text-dark-foreground dark:placeholder-dark-muted-foreground"
+            value={formData.password}
+            onChange={handleOnChange}
+            className="w-full p-2 border rounded bg-input text-foreground placeholder-muted-foreground"
+            required
           />
           <button
             type="submit"
-            className="bg-primary text-primary-foreground px-4 py-2 rounded w-full hover:bg-ring dark:bg-dark-primary dark:text-dark-primary-foreground dark:hover:bg-dark-ring"
+            className="bg-primary text-primary-foreground px-4 py-2 rounded w-full hover:bg-ring hover:bg-primary/80"
           >
             Login
           </button>
         </form>
         <p className="mt-3">
-           להרשמות לחץ 
-        <a href="/signup" className="text-purple-500"> כאן  </a>
+          להרשמות לחץ{" "}
+          <a href="/signup" className="text-primary hover:text-primary/70">
+            כאן
+          </a>
         </p>
         <br />
-        <button
-          onClick={onClose}
-          className="mt-1 text-destructive dark:text-dark-destructive "
-        >
+        <button onClick={onClose} className="mt-1 text-destructive">
           Close
         </button>
       </div>
