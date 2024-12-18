9bcb672b81d4f8e9c37f50bd07aa655d91ace8b1
ArielBitan <joekjoek33@gmail.com>
2024-12-18 14:03:05 +0200

    login authentication

--- server/routes/projectsRoutes.js
+++ server/routes/projectsRoutes.js
@@ -6,13 +6,14 @@ const {
   updateProjectById,
   deleteProjectById,
 } = require("../controllers/projectController");
+const auth = require("../middleware/authentication");
 
 const router = express.Router();
 
-router.post("/", createProject);
+router.post("/", auth, createProject);
 router.get("/", getAllProjects);
 router.get("/:id", getProjectById);
-router.put("/:id", updateProjectById);
-router.delete("/:id", deleteProjectById);
+router.put("/:id", auth, updateProjectById);
+router.delete("/:id", auth, deleteProjectById);
 
 module.exports = router;
