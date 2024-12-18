2589160cef75e91ef5ef7f6b2b06b2878a60e038
nimrod amos <nimrodamos100@gmail.com>
2024-12-18 09:25:35 +0200

    tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 console.log("baba");
 console.log("baba");
