65ba4e55382a1c350f1e3eeafcf45db7ba052764
ArielBitan <joekjoek33@gmail.com>
2024-12-18 14:27:29 +0200

    a

--- client/src/components/CarouselProject.tsx
+++ client/src/components/CarouselProject.tsx
@@ -27,11 +27,7 @@ const CarouselProject = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div className="relative p-6 rounded-lg shadow-lg max-w-7xl mx-auto">
-=======
-    <div className="relative p-6  rounded-lg shadow-lg max-w-7xl mx-auto">
->>>>>>> nimrod
       {/* כותרת הקרוסלה */}
       <h2 className="text-3xl font-bold text-center mb-6 text-primary">
         ✨ פרויקטים נבחרים ✨
