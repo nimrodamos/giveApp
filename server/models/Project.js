9846a75e65e6ba4ea9d36bb24b31e8fc9e025cc7
ArielBitan <joekjoek33@gmail.com>
2024-12-19 06:13:10 +0200

    a

--- server/models/Project.js
+++ server/models/Project.js
@@ -4,12 +4,15 @@ const ProjectSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
-    image: { type: String, default: "https://picsum.photos/500" },
+    image: {
+      type: String,
+      default: "../media/b3e0d1f1-62f6-4fe7-a150-d8358f2bc660.jpg",
+    },
     goal: { type: Number, required: true },
     current_amount: { type: Number, default: 0 },
     end_date: { type: Date },
     status: { type: String, default: "active" },
-    category: {type : String, default: "אחר"},
+    category: { type: String, default: "אחר" },
     user_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
