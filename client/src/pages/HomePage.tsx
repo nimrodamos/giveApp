7291db331e462973a77b457a3b67a01db7527fbc
OferS94 <ofersela94@gmail.com>
2024-12-19 04:09:02 +0200

    stastistics page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import CarouselProject from "@/components/CarouselProject";
 import StatisticsComponent from "@/components/StatisticsComponent";
-import InstagramMockup from "@/components/TestComp";
 const HomePage = () => {
   return (
     <div>
